05ec497a91d37ffb551a64d6742a7f8136a3eef7
Rok Marence <rokmarence@gmail.com>
2025-04-30 10:11:36 +0200

    random words added

--- wordgenerator.js
+++ wordgenerator.js
@@ -1,1 +1,23 @@
-console.log("Test")
+let startingPhrase=["The Most Expensive ", "5 Best ", "Top 10 "];
+let hook=["Yachts ", "Crusise Ships ","Boats "];
+let ending=["in the World ", "You Can Buy ", "to Charter"];
+
+let randomWordIndex = Math.floor(Math.random()*3);
+
+
+
+
+
+
+
+function randomTitle(){
+    // tu se bo zbral nakljucni index med 0 in 2
+
+    let index1= Math.floor(Math.random()*3);
+    let index2= Math.floor(Math.random()*3);
+    let index3= Math.floor(Math.random()*3);
+
+    return console.log(startingPhrase[index1]+hook[index2]+ending[index3]);
+}
+
+randomTitle()
